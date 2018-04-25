25f1bc1f0e82e4053ac38416a0cc43e7acd93b4d
Olexiy <o.m.letushev@gmail.com>
2018-04-25 18:41:37 +0300

    add EventEmitter class

---
+++ src/js/framework/EventEmitter.js
@@ -0,0 +1,31 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(eventName, fn) {
+    if (!this.events.hasOwnProperty(eventName)) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(fn);
+  }
+  
+  emit(eventName, data) {
+    const event = this.events[eventName];
+    if (event) {
+      event.forEach(fn => {
+        fn.call(null, data);
+      })
+    }
+  }
+
+  unsubscribe(eventName, fn) {
+    let event = this.events[eventName];
+    const index = event.indexOf(fn);
+    if (index != -1) {
+      event.splice(index, 1);
+    }
+  }
+}
+
+const EVENT_EMITTER = new EventEmitter();
